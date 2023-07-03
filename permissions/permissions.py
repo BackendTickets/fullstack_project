0d5e4faca52e8d917b1949d255e2127fe891133a
Рамазан <you@example.com>
2023-07-03 14:38:05 +0600

    token2

--- permissions/permissions.py
+++ permissions/permissions.py
@@ -3,6 +3,6 @@ from rest_framework.permissions import BasePermission
 
 class IsOwnerOrIsAdminUser(BasePermission):
     def has_object_permission(self, request, view, obj):
-        return (request.user.is_authenticated and request.user == obj.user) or (
+        return  (
             request.user.is_authenticated and request.user.is_staff
         )
