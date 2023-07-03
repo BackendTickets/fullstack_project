fdd4131f3b9c86d82308278a669ac4c930d27347
Рамазан <you@example.com>
2023-07-03 14:50:49 +0600

    page size

--- by_tickets/settings.py
+++ by_tickets/settings.py
@@ -161,7 +161,7 @@ REST_FRAMEWORK = {
         "rest_framework_simplejwt.authentication.JWTAuthentication",
         ),
         "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
-        "PAGE_SIZE": 6,
+        "PAGE_SIZE": 10,
         'DEFAULT_FILTER_BACKENDS': [
             'django_filters.rest_framework.DjangoFilterBackend',
             'rest_framework.filters.SearchFilter'
