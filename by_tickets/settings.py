1fa34a86b61eb3279185c011aa80efeb08bde697
Рамазан <you@example.com>
2023-07-03 14:15:27 +0600

    token update

--- by_tickets/settings.py
+++ by_tickets/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     "rest_framework",
+    'rest_framework.authtoken',
     "drf_yasg",
     "django_filters",
     "rest_framework_simplejwt",
