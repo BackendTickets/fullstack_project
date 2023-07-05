a51b47d8721bb739629baccdbf22cad861d23dc1
Рамазан <you@example.com>
2023-07-05 15:35:34 +0600

    permissions

--- applications/movies/serializers.py
+++ applications/movies/serializers.py
@@ -1,11 +1,22 @@
 from rest_framework import serializers
 from applications.genres.serializers import GenreSerializer
-
-
 from applications.movies.models import Movie
+from applications.genres.models import Genre
+
 class MovieSerializer(serializers.ModelSerializer):
-    genres = GenreSerializer(many=True, read_only=True)
+    genres = GenreSerializer(many=True)
 
     class Meta:
         model = Movie
         fields = '__all__'
+
+    def create(self, validated_data):
+        genres_data = validated_data.pop('genres', [])
+        movie = Movie.objects.create(**validated_data)
+
+        for genre_data in genres_data:
+            # Создание или получение объекта жанра из приложения genres
+            genre, _ = Genre.objects.get_or_create(**genre_data)
+            movie.genres.add(genre)
+
+        return movie
