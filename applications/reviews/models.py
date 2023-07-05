5c224f7ab17eb0f2b1e738c96056ed9d04b9d495
Рамазан <you@example.com>
2023-07-05 16:15:00 +0600

    comments

--- applications/reviews/models.py
+++ applications/reviews/models.py
@@ -5,7 +5,7 @@ from applications.movies.models import Movie
 
 
 class Comment(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, to_field='username')
     movies = models.ForeignKey(Movie, on_delete=models.CASCADE)
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
